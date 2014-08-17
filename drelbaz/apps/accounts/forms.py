7f3d1ca4c7bce4a5ac355205a3f2b6ccbfba2aeb
Kevin Go <keving@bixly.com>
2014-08-17 14:04:27 +0800

    Test fix for datetimefield form

--- drelbaz/apps/accounts/forms.py
+++ drelbaz/apps/accounts/forms.py
@@ -54,6 +54,7 @@ class LoginForm(forms.Form):
 
 
 class AppointmentForm(forms.ModelForm):
+    schedule = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%S',])
     class Meta:
         model = Appointment
 
